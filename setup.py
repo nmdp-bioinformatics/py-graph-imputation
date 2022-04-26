a19b50753da13291c21c8fe97efd150f6fc74085
pbashyal-nmdp <pbashyal@nmdp.org>
2022-04-26 16:12:57 -0500

    Update Development Status to 2 - Pre-Alpha

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     author_email="pbashyal@nmdp.org",
     python_requires=">=3.8",
     classifiers=[
-        "Development Status :: 1 - Pre-Alpha",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Natural Language :: English",
