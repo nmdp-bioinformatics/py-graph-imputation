eae4736dd51d1624c521fcf0ee70df73263be3fc
regev32 <regevel2006@gmail.com>
2025-09-11 15:23:29 +0300

    style: fix spacing around assignments and function calls

--- grim/grim.py
+++ grim/grim.py
@@ -54,7 +54,7 @@ def graph_freqs(conf_file="", for_em=False, em_pop=None):
     )
 
 
-def impute(conf_file="", hap_pop_pair = False, graph = None):
+def impute(conf_file="", hap_pop_pair=False, graph=None):
 
     project_dir_in_file, project_dir_graph = "", ""
     if conf_file == "":
@@ -70,7 +70,9 @@ def impute(conf_file="", hap_pop_pair = False, graph = None):
         project_dir_in_file = (
             os.path.dirname(os.path.realpath(__file__)).replace("/grim", "") + "/"
         )
-    graph = run_impute(conf_file, project_dir_graph, project_dir_in_file, hap_pop_pair, graph)
+    graph = run_impute(
+        conf_file, project_dir_graph, project_dir_in_file, hap_pop_pair, graph
+    )
     return graph
 
 
