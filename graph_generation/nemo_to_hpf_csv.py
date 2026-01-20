f61e35e1500c03e2efc03d7112cfe63d02ecd52e
Pradeep Bashyal <pbashyal@nmdp.org>
2026-01-20 05:57:05 -0600

    Fix linting issue.

--- graph_generation/nemo_to_hpf_csv.py
+++ graph_generation/nemo_to_hpf_csv.py
@@ -89,6 +89,6 @@ with open(hpf_file, mode="w") as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=",", quoting=csv.QUOTE_NONE)
     csv_writer.writerow(header)
     for pop_haplotype in pop_hap_combos:
-        (pop, haplotype) = pop_haplotype.split("-")
+        pop, haplotype = pop_haplotype.split("-")
         freq = pop_hap_combos[pop_haplotype]
         csv_writer.writerow([haplotype, pop, freq])
